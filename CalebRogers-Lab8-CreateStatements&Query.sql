49820b294910b3905044a366dafae8e05bab8754
Caleb-Rogers <70493310+Caleb-Rogers@users.noreply.github.com>
2020-11-12 00:12:30 -0500

    Add files via upload
    Caleb Rogers - 11/11/2020 - Lab 8: Normalization Two - This lab continues to exercise normalization through the break down of three areas of given data to eight detailed tables in BCNF.

---
+++ CalebRogers-Lab8-CreateStatements&Query.sql
@@ -0,0 +1,95 @@
+-- Caleb Rogers -- November 9th, 2020 -- Lab 8: Normalization Two
+
+DROP TABLE IF EXISTS ActorPhysical;
+DROP TABLE IF EXISTS MovieSales;
+DROP TABLE IF EXISTS MovieCast;
+DROP TABLE IF EXISTS Movies;
+DROP TABLE IF EXISTS Actors;
+DROP TABLE IF EXISTS Directors;
+DROP TABLE IF EXISTS People;
+DROP TABLE IF EXISTS Address;
+
+-- Create Statements --
+-- Address --
+CREATE TABLE Address (
+	zip integer NOT NULL,
+	cityOfZip text,
+	stateOfZip text,
+ PRIMARY KEY(zip)
+);
+
+-- People --
+CREATE TABLE People (
+	pid char(5) NOT NULL,
+	firstName text,
+	lastName text NOT NULL,
+	bday date,
+	spouseFn text,
+	spouseLN text,
+	addr1 text NOT NULL,
+	addr2 text,
+	zip integer NOT NULL references Address(zip),
+ PRIMARY KEY(pid)
+);
+
+-- Directors --
+CREATE TABLE Directors (
+	pid char(5) NOT NULL references People(pid),
+	directorAnniDate date,
+	directorSchool text,
+	favLensMaker text,
+ PRIMARY KEY(pid)
+);
+
+-- Actors --
+CREATE TABLE Actors (
+	pid char(5) NOT NULL references People(pid),
+	actorAnniDate date,
+	favColor text,
+ PRIMARY KEY(pid)
+);
+
+-- ActorPhysical --
+CREATE TABLE ActorPhysical (
+	actorID char(5) NOT NULL references Actors(pid),
+	eyeColor text,
+	hairColor text,
+	height integer,
+	weight integer,
+ PRIMARY KEY(actorID)
+);
+
+-- Movies --
+CREATE TABLE Movies (
+	movieID char(5) NOT NULL,
+	movieName text,
+	releaseYR integer,
+	MPAA integer,
+ PRIMARY KEY(movieID)
+);
+
+-- MovieCast --
+CREATE TABLE MovieCast (
+	actorID char(5) NOT NULL references Actors(pid),
+	directorID char(5) NOT NULL references Directors(pid),
+	movieID char(5) NOT NULL references Movies(movieID),
+ PRIMARY KEY(actorID, directorID)
+);
+
+-- MovieSales --
+CREATE TABLE MovieSales (
+	salesID char(5) NOT NULL references Movies(movieID),
+	domesticSales integer,
+	foreignSales integer,
+	DVD_BluRaySales integer,
+ PRIMARY KEY(salesID)
+);
+
+
+-- Query to show all the directors with whom actor “Roger Moore” has work --
+SELECT firstName, lastName
+FROM People P
+INNER JOIN Actors A ON P.pid = A.pid
+INNER JOIN Directors D ON P.pid = D.pid
+INNER JOIN MovieCast MC ON D.pid = MC.directorID
+WHERE lastName = 'Moore' and firstName = 'Roger';
