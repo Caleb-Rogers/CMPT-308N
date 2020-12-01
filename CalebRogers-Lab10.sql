6fc59737ea8e5896a8c91ec41909e60372fdeec9
Caleb-Rogers <70493310+Caleb-Rogers@users.noreply.github.com>
2020-12-01 17:21:04 -0500

    CalebRogers-Lab10.sql
    Caleb Rogers - December 1st, 2020 - Lab 10: Stored Procedures - This lab introduces our knowledge to stored procedures and practices writing functions for them.

---
+++ CalebRogers-Lab10.sql
@@ -0,0 +1,53 @@
+-- Caleb Rogers -- November 30th, 2020 -- Lab 10: Stored Procedures --
+
+--
+-- Write two functions (stored procedures) that take an integer course number as their only parameter:
+-- 1. function PreReqsFor(courseNum) - Returns the immediate prerequisites for the passed-in course number.
+-- 2. function IsPreReqFor(courseNum) - Returns the courses for which the passed-in course number is an immediate pre-requisite.
+--
+
+-- (1)
+create or replace function pre_reqs_for(integer, REFCURSOR) 
+returns refcursor as $$
+declare
+	course_num	integer	:= $1;
+	pre_reqs	REFCURSOR	:= $2;
+begin
+	open pre_reqs for
+		select num as prerequisites, name as className, credits
+		from courses
+		where num in (select preReqNum
+					        from Prerequisites
+					        where courseNum = course_num);
+	return pre_reqs;
+end;
+$$
+language plpgsql;
+
+-- example query --
+select pre_reqs_for(499, 'results');
+Fetch all from results;
+--
+
+-- (2)
+create or replace function is_pre_req_for(integer, REFCURSOR) 
+returns refcursor as $$
+declare
+	course_num	integer		:= $1;
+	pre_reqs	REFCURSOR	:= $2;
+begin
+	open pre_reqs for
+		select num as is_prerequisite_for, name as className, credits
+		from courses
+		where num in (select courseNum
+					   from Prerequisites
+					   where preReqNum = course_num);
+	return pre_reqs;
+end;
+$$
+language plpgsql;
+
+-- example query
+select is_pre_req_for(220, 'results');
+Fetch all from results;
+--
